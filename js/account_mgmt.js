30a5805b1c035d6958c83e0e409bcd02c5cb59ba
Sathwik Singari <sathwik@newtonsit.com.au>
2016-10-23 14:40:36 +1100

    Added opttion remove element

--- js/account_mgmt.js
+++ js/account_mgmt.js
@@ -56,3 +56,16 @@ $( document ).ready(function() {
 
     $('#login-name').html(getLoginName());
 });
+
+
+Element.prototype.remove = function() {
+    this.parentElement.removeChild(this);
+};
+
+NodeList.prototype.remove = HTMLCollection.prototype.remove = function() {
+    for(var i = this.length - 1; i >= 0; i--) {
+        if(this[i] && this[i].parentElement) {
+            this[i].parentElement.removeChild(this[i]);
+        }
+    }
+};
